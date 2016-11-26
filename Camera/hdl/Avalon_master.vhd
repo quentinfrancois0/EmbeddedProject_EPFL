1b970d42ef09c0e6aa9e4e3a6943a6c1fe7813c8
Quentin François <quentinfrancois0@gmail.com>
2016-11-26 18:51:35 +0100

    Modification of the slave and the master component

--- Camera/hdl/Avalon_master.vhd
+++ Camera/hdl/Avalon_master.vhd
@@ -1,17 +1,34 @@
 -- Design of a camera management device
--- Avalon slave unit
+-- Avalon master unit
 -- 
 -- Authors : Nicolas Berling & Quentin François
 -- Date : ??.11.2016
 --
--- Avalon slave for the camera management device
+-- Avalon master for the camera management device
 --
--- 6 address :
--- 	0x00: clock divider register
--- 	0x02: settings register | XXXXXXXM
-							-- M : mode, 1 = snapshot and 0 = continuous
--- 	0x03: Start adress memory
--- 	0x05: Length of the stored data in the memory
+-- ADRESSES
+-- nothing
+-- 
+-- INPUTS
+-- nReset <= extern
+-- Clock <= extern
+--
+-- AS_Start <= Slave
+-- AS_Start_Address <= Slave
+-- AS_Length <= Slave
+-- 
+-- FIFO_almost_empty <= FIFO
+-- FIFO_data <= FIFO
+-- 
+-- AM_WaitRequest <= Avalon Bus
+-- 
+-- OUTPUTS
+-- FIFO_Read_Access => FIFO
+--
+-- AM_Addr => Avalon Bus
+-- AM_Data => Avalon Bus
+-- AM_Write => Avalon Bus
+-- AM_BurstCount => Avalon Bus
 
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
@@ -26,56 +43,25 @@ ENTITY Avalon_master IS
 		AS_Start_Adress		: IN std_logic_vector (15 DOWNTO 0); 	-- Start Adress in the memory
 		AS_Length			: IN std_logic_vector (15 DOWNTO 0);	-- Length of the stored datas
 		
-		FIFO_almost_empty	: IN std_logic;							-- 1 when FIFO contains the burst length, 0 otherwise
-		FIFO_clk			: OUT std_logic;						-- clock sent to the FIFO = FPGA clock
+		FIFO_almost_empty	: IN std_logic;							-- 1 when FIFO contains at least the burst length, 0 otherwise
 		FIFO_Read_Access	: OUT std_logic;						-- 1 = information asked to the Fifo, 0 = no demand
 		FIFO_Data			: IN std_logic_vector (15 DOWNTO 0);	-- 1 pixel stored in the FIFO by hte camera controller
 		
 		AM_Addr				: OUT std_logic_vector (15 DOWNTO 0);	-- Adress sent on the Avalon bus
 		AM_Data				: OUT std_logic_vector (15 DOWNTO 0);	-- Datas sent on the Avalon bus
 		AM_Write			: OUT std_logic;						-- Pin write, 1 when the component wants to use the bus
+		AM_BurstCount		: OUT std_logic (7 DOWNTO 0);			-- Number of datas in one burst
 		AM_WaitRequest		: IN std_logic							-- Pin waitrequest which is 0 when the bus is available
+		
 	);
 END Avalon_master;
 
 ARCHITECTURE bhv OF Avalon_master IS
-	signal		iRegStart_Adress	: std_logic_vector (15 DOWNTO 0);	-- internal register for the memory start adress
-	signal		iRegLength			: std_logic_vector (15 DOWNTO 0);	-- internal register for the data stored length
-	signal		iRegStart			: std_logic;						-- internal register for the start command
-	signal		iRegCounterAdress	: std_logic_vector (31 DOWNTO 0);	-- internal phantom register which points on the current adress in the memory
-	TYPE		SM 	IS (WaitData, PickData, WaitBus, Transfer, EndOfBurst);
+	signal		iRegCounterAddress	: std_logic_vector (31 DOWNTO 0);	-- internal phantom register which points on the current adress in the memory
+	TYPE		SM 	IS (WaitData, PickData, Transfer, EndOfBurst);
 	Signal		SM_State			: SM;
 
 BEGIN
-
--- Process to write internal registers with the avalon slave
--- Synchronous access on rising edge of the FPGA's clock
-WriteProcess:
-Process(nReset, Clk)
-Begin
-	if nReset = '0' then	-- reset the writable registers when pushing the reset key
-		iRegStart			<= '0';
-		iRegStart_Adress	<= (others => '0');
-		iRegLength			<= (others => '0');
-	elsif rising_edge(Clk) then
-		if AS_Start = '1' then
-			iRegStart <= '1';
-			iRegStart_Adress <= AS_Start_Adress;
-			iRegLength <= AS_Length;
-		end if;
-	end if;
-end process WriteProcess;
-
--- Process to send the clock to the FIFO
-FIFOClk:
-Process(nReset, Clk)
-Begin
-	if nReset = '0' then
-		FIFO_clk <= '0';
-	elsif rising_edge(Clk) then
-		FIFO_clk <= Clk;
-	end if;
-end process FIFOClk;
 
 --  Process to take the datas in the FIFO and send it in the memory
 TransferDatas:
@@ -83,45 +69,56 @@ Process(nReset, Clk)
 Variable Indice : Integer Range 0 to 3;
 Begin
 	if nReset = '0' then
-		iRegCounterAdress <= iRegStart_Adress;
+		iRegCounterAddress <= (others => '0');
 		FIFO_Read_Access  <= '0';
 		AM_Addr			  <= (others => '0');
 		AM_Data			  <= (others => '0');
-		SM_State		  <= Waitdata;
+		AM_Write		  <= '0';
+		AM_BurstCount	  <= (others => '0');
+		SM_State		  <= WaitData;
 		Indice := 0;
 	elsif rising_edge(Clk) then
 		case SM_State is
-			when Waitdata =>
-				if FIFO_almost_empty = '0' then
-					FIFO_Read_Access <= '1';
+			when WaitData =>
+				if FIFO_almost_empty = '1' AND AS_Start = '1' then -- at least one burst in the FIFO and the Start at 1 for begin a burst
+					FIFO_Read_Access <= '1'; -- ask an info
 					SM_State <= PickData;
 				end if;
 			when PickData =>
 				if FIFO_Read_Access = '1' then
-					FIFO_Read_Access <= '0';
-					SM_State <= WaitBus;
-				end if;
-			when WaitBus =>
-				if AM_Write = '0' then
-					AM_Write <= '1';
-				elsif AM_WaitRequest = '0' then --bus available
+					FIFO_Read_Access <= '0'; -- ask an info to 0
 					SM_State <= Transfer;
 				end if;
 			when Transfer =>
-				AM_Data <= FIFO_Data;
-				AM_Addr <= iRegStart_Adress;
+				if AM_Write = '0' then
+					AM_Write <= '1'; -- say to the bus that he is waited
+				end if;
+				AM_Data <= FIFO_Data; -- FIFO data on the data bus
+				AM_Addr <= iRegStart_Adress + iRegCounterAddress; -- Start adress + current adress on the adress bus
+				AM_BurstCount <= "0X04";
+				if Indice < 3 then -- not the end of the burst
+					FIFO_Read_Access <= '1'; -- ask the next info
+				end if;
+				SM_State <= EndOfBurst;
 			when EndOfBurst =>
-				if Indice = 3 then -- end of the burst, let the bus and go to waitdata state
-					Indice := 0;
-					AM_Write <= '0';
-					AM_Data <= (others => '0');
-					AM_Addr <= (others => '0');
-					SM_State <= WaitData;
-				else
-					-- burst not ended -> transfer of next datas
-					Indice := Indice + 1;
-					FIFO_Read_Access <= '1';
-					SM_State <= PickData;
+				if AM_WaitRequest = '0' then --wait that the bus has transferred the data
+					if Indice = 3 then -- end of the burst, let the bus, reset the register and go to waitdata state
+						Indice := 0;
+						AM_Write <= '0';
+						AM_Data <= (others => '0');
+						AM_Addr <= (others => '0');
+						AM_BurstCount <= (others => '0');
+						SM_State <= WaitData;
+					else
+						-- burst not ended -> transfer of next datas
+						Indice := Indice + 1;
+						SM_State <= Transfer;
+						FIFO_Read_Access <= '0';
+					end if;
+					iRegCounterAdress <= std_logic_vector (unsigned (iRegCounterAddress) + 1); -- increase the iRegCounterAdress register
+					if iRegCounterAddress = AS_Length then -- when the iRegCounterAddress is equal to the data length (at the end of the 3 buffers), reset the counter to 0 to restart
+						iRegCounterAddress <= (others => '0');
+					end if;
 				end if;
 			when others => null;
 		end case;
--- Camera/hdl/Avalon_slave.vhd
+++ Camera/hdl/Avalon_slave.vhd
@@ -7,16 +7,26 @@
 -- Avalon slave for the camera management device
 --
 -- ADRESSES
--- 	0x00: Start adress memory
--- 	0x02: Length of the stored data in the memory
+--  0x00: Start information
+--  ---- ---X : X = Start information, 1 = ON, 0 = OFF
+-- 	0x01: Start address of the stored datas in the memory
+-- 	0x03: Length of the stored data in the memory
 -- 
 -- INPUTS
--- Clk <= FPGA Clock 50 MHz
+-- nReset <= extern
+-- Clock <= extern
+--
+-- Addr <= Avalon Bus
+-- R <= Avalon Bus
+-- W <= Avalon Bus
+-- WData <= Avalon Bus
 -- 
 -- OUTPUTS
--- Clkout => Camera Clock 25 MHz
--- Start_Adress => Master, start_adress
--- Length => Master, length
+-- Start_Address => Master
+-- Length => Master
+-- Start information => Master, Camera Controller
+--
+-- RData => Avalon Bus
 
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
@@ -26,22 +36,24 @@ ENTITY Avalon_slave IS
 	PORT(
 		nReset			: IN std_logic;							-- nReset input
 		Clk				: IN std_logic;							-- clock input
+		
 		Addr			: IN std_logic_vector (1 DOWNTO 0);		-- address bus
 		R				: IN std_logic;							-- read enabler
 		W				: IN std_logic;							-- write enabler
 		RData			: OUT std_logic_vector (7 DOWNTO 0);	-- data bus (read)
 		WData			: IN std_logic_vector (7 DOWNTO 0);		-- data bus (write)
-		Clkout			: OUT std_logic;						-- Clock output
-		Start_Adress	: OUT std_logic_vector (15 DOWNTO 0); 	-- Start Adress in the memory
-		Length			: OUT std_logic_vector (15 DOWNTO 0)	-- Length of the stored datas
+		
+		Start_Address	: OUT std_logic_vector (15 DOWNTO 0); 	-- Start Adress in the memory
+		Length			: OUT std_logic_vector (15 DOWNTO 0);	-- Length of the stored datas
+		Start			: OUT std_logic_vector					-- Start information
 	);
 END Avalon_slave;
 
-ARCHITECTURE bhv OF Avalon_slave IS
-	signal		iRegCountEnable		: std_logic_vector;					-- internal phantom counter enabler register
-	signal		iRegOut				: std_logic;						-- internal phantom out register
+ARCHITECTURE bhv OF Avalon_slave IS	
 	signal		iRegRead			: std_logic;						-- internal phantom read register
-	signal		iRegStart_Adress	: std_logic_vector (15 DOWNTO 0);	-- internal register for the memory start adress
+	signal		iFlagSettings		: std_logic (3 DOWNTO 0);			-- internal phantom flag in order to avoid seetings modification after configuration
+	signal		iRegStart			: std_logic;						-- internal register for the start information
+	signal		iRegStart_Address	: std_logic_vector (15 DOWNTO 0);	-- internal register for the memory start adress
 	signal		iRegLength			: std_logic_vector (15 DOWNTO 0);	-- internal register for the data stored length
 
 BEGIN
@@ -52,15 +64,34 @@ WriteProcess:
 Process(nReset, Clk)
 Begin
 	if nReset = '0' then	-- reset the four writable registers when pushing the reset key
-		iRegStart_Adress	<= (others => '0');
+		iRegStart			<= '0';
+		iRegStart_Address	<= (others => '0');
 		iRegLength			<= (others => '0');
+		iFlagSettings		<= (others => '0');
 	elsif rising_edge(Clk) then
 		if W = '1' then
 			case Addr is
-				when "00" => iRegStart_Adress (7 DOWNTO 0)		<= WData;
-				when "01" => iRegStart_Adress (15 DOWNTO 8)		<= WData;
-				when "10" => iRegLength (7 DOWNTO 0)			<= WData;
-				when "11" => iRegLength (15 DOWNTO 8)			<= WData;
+				when "000" => iRegStart	<= WData(0);
+				when "001" => 
+					if iFlagSettings(0) = '0' then
+						iRegStart_Address (7 DOWNTO 0)	<= WData;
+						iFlagSettings(0) <= '1';
+					end if;
+				when "010" => 
+					if iFlagSettings(1) = '0' then
+						iRegStart_Address (15 DOWNTO 8)	<= WData;
+						iFlagSettings(1) <= '1';
+					end if;
+				when "011" => 
+					if iFlagSettings(2) = '0' then
+						iRegLength (7 DOWNTO 0)			<= WData;
+						iFlagSettings(2) <= '1';
+					end if;
+				when "100" => 
+					if iFlagSettings(3) = '0' then
+						iRegLength (15 DOWNTO 8)		<= WData;
+						iFlagSettings(3) <= '1';
+					end if;
 				when others => null;
 			end case;
 		end if;
@@ -81,45 +112,34 @@ end process WaitRead;
 -- Process to read internal registers through Avalon bus interface
 -- Synchronous access on rising edge of the FPGA's clock with 1 wait
 ReadProcess:
-Process(iRegRead, Addr, iRegStart_Adress, iRegLength)
+Process(iRegRead, Addr, iRegStart_Address, iRegLength)
 Begin
 	RData <= (others => '0');	-- reset the data bus (read) when not used
 	if iRegRead = '1' then
 		case Addr is
-			when "00" => RData <= iRegStart_Adress (7 DOWNTO 0);
-			when "01" => RData <= iRegStart_Adress (15 DOWNTO 8);
-			when "10" => RData <= iRegLength (7 DOWNTO 0);
-			when "11" => RData <= iRegLength (15 DOWNTO 8);
+			when "000" => RData <= iRegStart;
+			when "001" => RData <= iRegStart_Address (7 DOWNTO 0);
+			when "010" => RData <= iRegStart_Address (15 DOWNTO 8);
+			when "011" => RData <= iRegLength (7 DOWNTO 0);
+			when "100" => RData <= iRegLength (15 DOWNTO 8);
 			when others => null;
 		end case;
 	end if;
 end process ReadProcess;
 
--- Process to divide the clock and to increment the internal counter register
-ClkDivider:
+-- Process to update the output towards the master and the camera controller
+UpdateOutput:
 Process(nReset, Clk)
 Begin
-	if nReset = '0' then	-- reset the internal phantom counter enabler register when pushing the reset key
-		iRegCountEnable	<= (others => '0');
-	elsif rising_edge(clk) then -- toggle the iRegCountEnable in order to divide the in clock by 2, FPGA clock = 50 MHz, Camera clock = 25 MHz
-		if iRegCountEnable ='0' then
-			iRegCountEnable <= '1';
-		else
-			iRegCountEnable <= '0';
-		end if;
+	if nReset = '0' then
+		Start_Address <= (others => '0');
+		Length <= (others => '0');
+		Start	<= '0';
+	elsif rising_edge(Clk) then
+		Start_Address <= iRegStart_Address;
+		Length <= iRegLength;
+		Start <= iRegStart;
 	end if;
-end process ClkDivider;
-
-ClkOutUpdate:
-Process(iRegCountEnable, nReset)
-Begin
-	if nReset = '0' then -- reset the output when pushing the reset key
-		Clkout <= '0';
-	elsif iRegCountEnable = '1' then
-		Clkout <= '1';
-	else
-		CLkout <= '0';
-	end if;
-end process ClkOutUpdate;
+end process UpdateOutput;
 
 END bhv;
