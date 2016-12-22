f2886200a0cab0864bcd8afe0fed53674c2ec28e
Quentin François <quentinfrancois0@gmail.com>
2016-12-22 17:52:40 +0100

    Avalon slave update 2

--- Camera/hdl/Avalon_slave.vhd
+++ Camera/hdl/Avalon_slave.vhd
@@ -7,26 +7,26 @@
 -- Avalon slave for the camera management device
 --
 -- ADRESSES
---  0x00: Start information
---  ---- ---X : X = Start information, 1 = ON, 0 = OFF
--- 	0x01: Start address of the stored datas in the memory
--- 	0x05: Length of the stored data in the memory
+--  0x00: AS_Start information
+--  ---- ---X : X = AS_Start information, 1 = ON, 0 = OFF
+-- 	0x01: AS_Start address of the stored datas in the memory
+-- 	0x05: AS_Length of the stored data in the memory
 -- 
 -- INPUTS
--- nReset <= extern
+-- AS_nReset <= extern
 -- Clock <= extern
 --
--- Addr <= Avalon Bus
--- R <= Avalon Bus
--- W <= Avalon Bus
--- WData <= Avalon Bus
+-- AS_Address <= Avalon Bus
+-- AS_ReadEnable <= Avalon Bus
+-- AS_WriteEnable <= Avalon Bus
+-- AS_WriteData <= Avalon Bus
 -- 
 -- OUTPUTS
--- Start_Address => Master
--- Length => Master
--- Start information => Master, Camera Controller
+-- AS_StartAddress => Master
+-- AS_Length => Master
+-- AS_Start information => Master, Camera Controller
 --
--- RData => Avalon Bus
+-- AS_ReadData => Avalon Bus
 
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
@@ -34,7 +34,7 @@ USE ieee.numeric_std.all;
 
 ENTITY Avalon_slave IS
 	PORT(
-		AS_nReset			: IN std_logic;							-- nReset input
+		AS_nReset			: IN std_logic;							-- AS_nReset input
 		AS_Clk				: IN std_logic;							-- clock input
 		
 		AS_Address			: IN std_logic_vector (3 DOWNTO 0);		-- address bus
@@ -43,114 +43,102 @@ ENTITY Avalon_slave IS
 		AS_ReadData			: OUT std_logic_vector (7 DOWNTO 0);	-- data bus (read)
 		AS_WriteData		: IN std_logic_vector (7 DOWNTO 0);		-- data bus (write)
 		
-		AS_StartAddress		: OUT std_logic_vector (31 DOWNTO 0); 	-- Start Adress in the memory
-		AS_Length			: OUT std_logic_vector (31 DOWNTO 0);	-- Length of the stored datas
-		AS_Start			: OUT std_logic;						-- Start information
-		AS_Status			: IN std_logic;							-- 1 when the image has been written to the memory
+		AS_StartAddress		: OUT std_logic_vector (31 DOWNTO 0); 	-- AS_Start Adress in the memory
+		AS_Length			: OUT std_logic_vector (31 DOWNTO 0);	-- AS_Length of the stored datas
+		AS_Start			: OUT std_logic;						-- AS_Start information
+		AS_Status				: IN std_logic;							-- 1 when the image has been written to the memory
 	);
 END Avalon_slave;
 
 ARCHITECTURE bhv OF Avalon_slave IS	
-	signal		iRegRead			: std_logic;						-- internal phantom read register
 	signal		iFlagSettings		: std_logic_vector (7 DOWNTO 0);	-- internal phantom flag in order to avoid seetings modification after configuration
-	signal		iRegStart			: std_logic;						-- internal register for the start information
-	signal		iRegStart_Address	: std_logic_vector (31 DOWNTO 0);	-- internal register for the memory start adress
-	signal		iRegLength			: std_logic_vector (31 DOWNTO 0);	-- internal register for the data stored length
-	signal		iRegStatus			: std_logic_vector (2 DOWNTO 0);	-- internal register for the writing status of each buffer
+	signal		iRegStart			: std_logic;						-- internal register for the AS_Start information
+	signal		iRegStart_Address	: std_logic_vector (31 DOWNTO 0);	-- internal register for the memory AS_Start adress
+	signal		iRegLength			: std_logic_vector (31 DOWNTO 0);	-- internal register for the data stored AS_Length
+	signal		iRegStatus			: std_logic_vector (2 DOWNTO 0);	-- internal register for the writing AS_Status of each buffer
 
 BEGIN
 
 -- Process to write internal registers through Avalon bus interface
 -- Synchronous access on rising edge of the FPGA's clock
 WriteProcess:
-Process(nReset, Clk)
+Process(AS_nReset, AS_Clk)
 Begin
-	if nReset = '0' then	-- reset the four writable registers when pushing the reset key
+	if AS_nReset = '0' then	-- reset the four writable registers when pushing the reset key
 		iRegStart			<= '0';
 		iRegStart_Address	<= (others => '0');
 		iRegLength			<= (others => '0');
 		iFlagSettings		<= (others => '0');
 		iRegStatus			<= (others => '0');
-	elsif rising_edge(Clk) then
-		if W = '1' then
-			case Addr is
-				when X"0" => iRegStart	<= WData(0);
+	elsif rising_edge(AS_Clk) then
+		if AS_WriteEnable = '1' then
+			case AS_Address is
+				when X"0" => iRegStart	<= AS_WriteData(0);
 				when X"1" => 
 					if iFlagSettings(0) = '0' then
-						iRegStart_Address (7 DOWNTO 0)	<= WData;
+						iRegStart_Address (7 DOWNTO 0)	<= AS_WriteData;
 						iFlagSettings(0) <= '1';
 					end if;
 				when X"2" => 
 					if iFlagSettings(1) = '0' then
-						iRegStart_Address (15 DOWNTO 8)	<= WData;
+						iRegStart_Address (15 DOWNTO 8)	<= AS_WriteData;
 						iFlagSettings(1) <= '1';
 					end if;
 				when X"3" => 
 					if iFlagSettings(2) = '0' then
-						iRegStart_Address (23 DOWNTO 16)	<= WData;
+						iRegStart_Address (23 DOWNTO 16)	<= AS_WriteData;
 						iFlagSettings(2) <= '1';
 					end if;
 				when X"4" => 
 					if iFlagSettings(3) = '0' then
-						iRegStart_Address (31 DOWNTO 24)	<= WData;
+						iRegStart_Address (31 DOWNTO 24)	<= AS_WriteData;
 						iFlagSettings(3) <= '1';
 					end if;
 				when X"5" => 
 					if iFlagSettings(4) = '0' then
-						iRegLength (7 DOWNTO 0)			<= WData;
+						iRegLength (7 DOWNTO 0)			<= AS_WriteData;
 						iFlagSettings(4) <= '1';
 					end if;
 				when X"6" => 
 					if iFlagSettings(5) = '0' then
-						iRegLength (15 DOWNTO 8)		<= WData;
+						iRegLength (15 DOWNTO 8)		<= AS_WriteData;
 						iFlagSettings(5) <= '1';
 					end if;
 				when X"7" => 
 					if iFlagSettings(6) = '0' then
-						iRegLength (23 DOWNTO 16)		<= WData;
+						iRegLength (23 DOWNTO 16)		<= AS_WriteData;
 						iFlagSettings(6) <= '1';
 					end if;
 				when X"8" => 
 					if iFlagSettings(7) = '0' then
-						iRegLength (31 DOWNTO 24)		<= WData;
+						iRegLength (31 DOWNTO 24)		<= AS_WriteData;
 						iFlagSettings(7) <= '1';
 					end if;
-				when X"9" => iRegStatus <= WData (2 DOWNTO 0);
+				when X"9" => iRegStatus <= AS_WriteData (2 DOWNTO 0);
 				when others => null;
 			end case;
 		end if;
 	end if;
 end process WriteProcess;
 
--- Process to wait one rising edge before reading the internal registers
-WaitRead:
-Process(nReset, Clk)
-Begin
-	if nReset = '0' then	-- reset the internal phantom read register when pushing the reset key
-		iRegRead <= '0';
-	elsif rising_edge(Clk) then
-		iRegRead <= R;
-	end if;
-end process WaitRead;
-
 -- Process to read internal registers through Avalon bus interface
 -- Synchronous access on rising edge of the FPGA's clock with 1 wait
 ReadProcess:
-Process(iRegRead, Addr, iRegStart_Address, iRegLength)
+Process(AS_ReadEnable, AS_Address, iRegStart_Address, iRegLength)
 Begin
-	RData <= (others => '0');	-- reset the data bus (read) when not used
-	if iRegRead = '1' then
-		case Addr is
-			when X"0" => RData(0) 				<= iRegStart;
-			when X"1" => RData 					<= iRegStart_Address (7 DOWNTO 0);
-			when X"2" => RData 					<= iRegStart_Address (15 DOWNTO 8);
-			when X"3" => RData 					<= iRegStart_Address (23 DOWNTO 16);
-			when X"4" => RData 					<= iRegStart_Address (31 DOWNTO 24);
-			when X"5" => RData 					<= iRegLength (7 DOWNTO 0);
-			when X"6" => RData 					<= iRegLength (15 DOWNTO 8);
-			when X"7" => RData 					<= iRegLength (23 DOWNTO 16);
-			when X"8" => RData 					<= iRegLength (31 DOWNTO 24);
-			when X"9" => RData (2 DOWNTO 0)		<= iRegStatus;
+	AS_ReadData <= (others => '0');	-- reset the data bus (read) when not used
+	if AS_ReadEnable = '1' then
+		case AS_Address is
+			when X"0" => AS_ReadData(0) 				<= iRegStart;
+			when X"1" => AS_ReadData 					<= iRegStart_Address (7 DOWNTO 0);
+			when X"2" => AS_ReadData 					<= iRegStart_Address (15 DOWNTO 8);
+			when X"3" => AS_ReadData 					<= iRegStart_Address (23 DOWNTO 16);
+			when X"4" => AS_ReadData 					<= iRegStart_Address (31 DOWNTO 24);
+			when X"5" => AS_ReadData 					<= iRegLength (7 DOWNTO 0);
+			when X"6" => AS_ReadData 					<= iRegLength (15 DOWNTO 8);
+			when X"7" => AS_ReadData 					<= iRegLength (23 DOWNTO 16);
+			when X"8" => AS_ReadData 					<= iRegLength (31 DOWNTO 24);
+			when X"9" => AS_ReadData (2 DOWNTO 0)		<= iRegStatus;
 			when others => null;
 		end case;
 	end if;
@@ -158,16 +146,16 @@ end process ReadProcess;
 
 -- Process to update the output towards the master and the camera controller
 UpdateOutput:
-Process(nReset, Clk)
+Process(AS_nReset, AS_Clk)
 Begin
-	if nReset = '0' then
-		Start_Address <= (others => '0');
-		Length <= (others => '0');
-		Start	<= '0';
-	elsif rising_edge(Clk) then
-		Start_Address <= iRegStart_Address;
-		Length <= iRegLength;
-		Start <= iRegStart;
+	if AS_nReset = '0' then
+		AS_StartAddress <= (others => '0');
+		AS_Length <= (others => '0');
+		AS_Start	<= '0';
+	elsif rising_edge(AS_Clk) then
+		AS_StartAddress <= iRegStart_Address;
+		AS_Length <= iRegLength;
+		AS_Start <= iRegStart;
 	end if;
 end process UpdateOutput;
 
--- Camera/hdl/testbench_Avalon_slave.vhd
+++ Camera/hdl/testbench_Avalon_slave.vhd
@@ -35,36 +35,36 @@ ARCHITECTURE bhv OF testbench IS
 -- The system to test under simulation
 component Avalon_slave is
 	PORT(
-		nReset			: IN std_logic;							-- nReset input
-		Clk				: IN std_logic;							-- clock input
+		AS_nReset			: IN std_logic;							-- AS_nReset input
+		AS_Clk				: IN std_logic;							-- clock input
 		
-		Addr			: IN std_logic_vector (3 DOWNTO 0);		-- address bus
-		R				: IN std_logic;							-- read enabler
-		W				: IN std_logic;							-- write enabler
-		RData			: OUT std_logic_vector (7 DOWNTO 0);	-- data bus (read)
-		WData			: IN std_logic_vector (7 DOWNTO 0);		-- data bus (write)
+		AS_Address			: IN std_logic_vector (3 DOWNTO 0);		-- address bus
+		AS_ReadEnable		: IN std_logic;							-- read enabler
+		AS_WriteEnable		: IN std_logic;							-- write enabler
+		AS_ReadData			: OUT std_logic_vector (7 DOWNTO 0);	-- data bus (read)
+		AS_WriteData		: IN std_logic_vector (7 DOWNTO 0);		-- data bus (write)
 		
-		Start_Address	: OUT std_logic_vector (31 DOWNTO 0); 	-- Start Adress in the memory
-		Length			: OUT std_logic_vector (31 DOWNTO 0);	-- Length of the stored datas
-		Start			: OUT std_logic	;			-- Start information
-		Status			: INOUT std_logic_vector (2 DOWNTO 0)
+		AS_StartAddress		: OUT std_logic_vector (31 DOWNTO 0); 	-- AS_Start Adress in the memory
+		AS_Length			: OUT std_logic_vector (31 DOWNTO 0);	-- AS_Length of the stored datas
+		AS_Start			: OUT std_logic	;			-- AS_Start information
+		AS_Status			: INOUT std_logic_vector (2 DOWNTO 0)
 	);
 end component;
 
 -- The interconnection signals :
-signal nReset			: std_logic := '1';
-signal Clk				: std_logic := '0';
+signal AS_nReset			: std_logic := '1';
+signal AS_Clk				: std_logic := '0';
 
-signal Addr				: std_logic_vector (3 DOWNTO 0) := X"0";
-signal R				: std_logic := '0';
-signal W				: std_logic := '0';
-signal RData			: std_logic_vector (7 DOWNTO 0) := X"00";
-signal WData			: std_logic_vector (7 DOWNTO 0) := X"00";
+signal AS_Address			: std_logic_vector (3 DOWNTO 0) := X"0";
+signal AS_ReadEnable		: std_logic := '0';
+signal AS_WriteEnable		: std_logic := '0';
+signal AS_ReadData			: std_logic_vector (7 DOWNTO 0) := X"00";
+signal AS_WriteData			: std_logic_vector (7 DOWNTO 0) := X"00";
 
-signal Start_Address	: std_logic_vector (31 DOWNTO 0) := X"00000000";
-signal Length			: std_logic_vector (31 DOWNTO 0) := X"00000000";
-signal Start			: std_logic := '0';
-signal Status			: std_logic_vector (2 DOWNTO 0) := "000";
+signal AS_StartAddress		: std_logic_vector (31 DOWNTO 0) := X"00000000";
+signal AS_Length			: std_logic_vector (31 DOWNTO 0) := X"00000000";
+signal AS_Start				: std_logic := '0';
+signal AS_Status			: std_logic_vector (2 DOWNTO 0) := "000";
 
 signal end_sim	: boolean := false;
 constant HalfPeriod  : TIME := 10 ns;  -- clk_FPGA = 50 MHz -> T_FPGA = 20ns -> T/2 = 10 ns
@@ -72,19 +72,19 @@ constant HalfPeriod  : TIME := 10 ns;  -- clk_FPGA = 50 MHz -> T_FPGA = 20ns -> 
 BEGIN 
 DUT : Avalon_slave	-- Component to test as Device Under Test       
 	Port MAP(	-- from component => signal in the architecture
-		nReset => nReset,
-		Clk => Clk,
+		AS_nReset => AS_nReset,
+		AS_Clk => AS_Clk,
 		
-		Addr => Addr,
-		R => R,
-		W => W,
-		RData => RData,
-		WData => WData,
+		AS_Address => AS_Address,
+		AS_ReadEnable => AS_ReadEnable,
+		AS_WriteEnable => AS_WriteEnable,
+		AS_ReadData => AS_ReadData,
+		AS_WriteData => AS_WriteData,
 		
-		Start_Address => Start_Address,
-		Length => Length,
-		Start => Start,
-		Status => Status
+		AS_StartAddress => AS_StartAddress,
+		AS_Length => AS_Length,
+		AS_Start => AS_Start,
+		AS_Status => AS_Status
 	);
 
 -- Process to generate the clock during the whole simulation
@@ -92,9 +92,9 @@ clk_process :
 Process
 Begin
 	if not end_sim then	-- generate the clocc while simulation is running
-		Clk <= '0';
+		AS_Clk <= '0';
 		wait for HalfPeriod;
-		Clk <= '1';
+		AS_Clk <= '1';
 		wait for HalfPeriod;
 	else	-- when the simulation is ended, just wait
 		wait;
@@ -108,46 +108,46 @@ Process
 	-- Procedure to toggle the reset
 	Procedure toggle_reset is
 	Begin
-		wait until rising_edge(clk);
-		nReset <= '0';
+		wait until rising_edge(AS_Clk);
+		AS_nReset <= '0';
 		
-		wait until rising_edge(clk);
-		nReset <= '1';
+		wait until rising_edge(AS_Clk);
+		AS_nReset <= '1';
 	end procedure toggle_reset;
 
 	-- Procedure to write a register, inputs are (address, data_to_write)
 	Procedure write_register(addr_write: std_logic_vector; data: std_logic_vector) is
 	Begin
-		wait until rising_edge(clk);	-- write between two consecutive rising edges of the clock
-		W <= '1';
-		Addr <= addr_write;
-		WData <= data;
+		wait until rising_edge(AS_Clk);	-- write between two consecutive rising edges of the clock
+		AS_WriteEnable <= '1';
+		AS_Address <= addr_write;
+		AS_WriteData <= data;
 		
-		wait until rising_edge(clk);	-- then reset everything
-		W <= '0';
-		Addr <= X"0";
-		WData <= X"00";
+		wait until rising_edge(AS_Clk);	-- then reset everything
+		AS_WriteEnable <= '0';
+		AS_Address <= X"0";
+		AS_WriteData <= X"00";
 	end procedure write_register;
 
 	-- Procedure to read a register, input is (address)
 	Procedure read_register(addr_read: std_logic_vector) is
 	Begin
-		wait until rising_edge(clk);	-- set the read access, so the internal phantom read register will be set to 1 on the next rising edge of the clock
-		R <= '1';
+		wait until rising_edge(AS_Clk);	-- set the read access, so the internal phantom read register will be set to 1 on the next rising edge of the clock
+		AS_ReadEnable <= '1';
 		
-		wait until rising_edge(clk);	-- now the internal phantom read register will be set to 1, we can read the register
-		Addr <= addr_read;
+		wait until rising_edge(AS_Clk);	-- now the internal phantom read register will be set to 1, we can read the register
+		AS_Address <= addr_read;
 		
-		wait until rising_edge(clk);	-- then reset everything
-		R <= '0';
-		Addr <= X"0";
+		wait until rising_edge(AS_Clk);	-- then reset everything
+		AS_ReadEnable <= '0';
+		AS_Address <= X"0";
 	end procedure read_register;
 
 Begin
 	-- Toggling the reset
 	toggle_reset;
 	
-	-- Writing start_address = 20 = 0x00000014
+	-- Writing AS_StartAddress = 20 = 0x00000014
 	write_register(X"1", X"14");
 	
 	-- Writing start_adress = 300 = 0x1010012C -> 0x10100114
@@ -156,37 +156,37 @@ Begin
 	write_register(X"3", X"10");
 	write_register(X"4", X"10");
 	
-	-- Writing length = 32 = 0x20204020
+	-- Writing AS_Length = 32 = 0x20204020
 	write_register(X"5", X"20");
 	write_register(X"6", X"40");
 	write_register(X"7", X"20");
 	write_register(X"8", X"20");
 	
-	-- Writing status of buffers
+	-- Writing AS_Status of buffers
 	write_register(X"9", X"05");
 	
-	-- Writing Start information = 1
+	-- Writing AS_Start information = 1
 	write_register(X"0", X"01");
 	
-	-- Reading Start information
+	-- Reading AS_Start information
 	read_register(X"0");
 	
-	-- Writing Start information = 0
+	-- Writing AS_Start information = 0
 	write_register(X"0", X"00");
 	
-	-- Reading the Start_Address(0x10100114)
+	-- Reading the AS_StartAddress(0x10100114)
 	read_register(X"1");
 	read_register(X"2");
 	read_register(X"3");
 	read_register(X"4");
 	
-	-- Reading the Length (320 = 0x20204020)
+	-- Reading the AS_Length (320 = 0x20204020)
 	read_register(X"5");
 	read_register(X"6");
 	read_register(X"7");
 	read_register(X"8");
 	
-	-- Reading status of buffers
+	-- Reading AS_Status of buffers
 	read_register(X"9");
 	
 	-- Set end_sim to "true", so the clock generation stops
