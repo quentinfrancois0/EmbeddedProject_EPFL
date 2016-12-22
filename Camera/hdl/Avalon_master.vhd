496e4bedd104388bde160f8e8ffff1a6acd4b78f
Astreos <spideraider@gmail.com>
2016-12-22 17:41:09 +0100

    Name change of the Avalon master

--- Camera/hdl/Avalon_master.vhd
+++ Camera/hdl/Avalon_master.vhd
@@ -10,24 +10,24 @@
 -- nothing
 -- 
 -- INPUTS
--- nReset <= extern
+-- AM_nReset <= extern
 -- Clock <= extern
 --
--- AS_Start <= Slave
--- AS_Start_Address <= Slave
--- AS_Length <= Slave
+-- AM_Start <= Slave
+-- AS_StartAddress <= Slave
+-- AM_Length <= Slave
 -- 
--- FIFO_number_words <= FIFO
+-- AM_UsedWords <= FIFO
 -- FIFO_data <= FIFO
 -- 
 -- AM_WaitRequest <= Avalon Bus
 -- 
 -- OUTPUTS
--- FIFO_Read_Access => FIFO
+-- AM_ReadAccess => FIFO
 --
--- AM_Addr => Avalon Bus
--- AM_Data => Avalon Bus
--- AM_Write => Avalon Bus
+-- AM_MemoryAddress => Avalon Bus
+-- AM_AvalonData => Avalon Bus
+-- AM_WriteRequest => Avalon Bus
 -- AM_BurstCount => Avalon Bus
 
 LIBRARY ieee;
@@ -36,21 +36,21 @@ USE ieee.numeric_std.all;
 
 ENTITY Avalon_master IS
 	PORT(
-		nReset				: IN std_logic;								-- nReset input
-		Clk					: IN std_logic;								-- clock input
+		AM_nReset				: IN std_logic;								-- AM_nReset input
+		AM_Clk					: IN std_logic;								-- clock input
 		
-		AS_Start			: IN std_logic;								-- Start command
-		AS_Start_Address	: IN std_logic_vector (31 DOWNTO 0); 	-- Start Adress in the memory
-		AS_Length			: IN std_logic_vector (31 DOWNTO 0);	-- Length of the stored datas
-		AS_Status			: OUT std_logic;						-- 1 when the image has been written to the memory
+		AM_Start			: IN std_logic;								-- Start command
+		AS_StartAddress	: IN std_logic_vector (31 DOWNTO 0); 	-- Start Adress in the memory
+		AM_Length			: IN std_logic_vector (31 DOWNTO 0);	-- Length of the stored datas
+		AM_Status			: OUT std_logic;						-- 1 when the image has been written to the memory
 		
-		FIFO_number_words	: IN std_logic_vector (7 DOWNTO 0);		-- number of 32 bits words
-		FIFO_Read_Access	: OUT std_logic;						-- 1 = information asked to the Fifo, 0 = no demand
-		FIFO_Data			: IN std_logic_vector (31 DOWNTO 0);	-- 1 pixel stored in the FIFO by hte camera controller
+		AM_UsedWords	: IN std_logic_vector (7 DOWNTO 0);		-- number of 32 bits words
+		AM_ReadAccess	: OUT std_logic;						-- 1 = information asked to the Fifo, 0 = no demand
+		AM_FIFOData			: IN std_logic_vector (31 DOWNTO 0);	-- 1 pixel stored in the FIFO by hte camera controller
 		
-		AM_Addr				: OUT std_logic_vector (31 DOWNTO 0);	-- Adress sent on the Avalon bus
-		AM_Data				: OUT std_logic_vector (31 DOWNTO 0);	-- Datas sent on the Avalon bus
-		AM_Write			: OUT std_logic;						-- Pin write, 1 when the component wants to use the bus
+		AM_MemoryAddress	: OUT std_logic_vector (31 DOWNTO 0);	-- Adress sent on the Avalon bus
+		AM_AvalonData				: OUT std_logic_vector (31 DOWNTO 0);	-- Datas sent on the Avalon bus
+		AM_WriteRequest			: OUT std_logic;						-- Pin write, 1 when the component wants to use the bus
 		AM_BurstCount		: OUT std_logic_vector (7 DOWNTO 0);	-- Number of datas in one burst
 		AM_WaitRequest		: IN std_logic							-- Pin waitrequest which is 0 when the bus is available
 	);
@@ -58,7 +58,7 @@ END Avalon_master;
 
 ARCHITECTURE bhv OF Avalon_master IS
 	constant    BURSTCOUNT_LENGTH : positive := 16;
-	constant    ADDR_INCREMENT : natural := (AM_Data'length / 8) * BURSTCOUNT_LENGTH;
+	constant    ADDR_INCREMENT : natural := (AM_AvalonData'length / 8) * BURSTCOUNT_LENGTH;
 	
 	signal		iRegAlmostEmpty				: std_logic;						-- internal phantom register which says if there is at least a burst in the FIFO
 	signal		iRegCounterAddress, next_iRegCounterAddress			: std_logic_vector (31 DOWNTO 0);	-- internal phantom register which points on the current adress in the memory
@@ -70,9 +70,9 @@ ARCHITECTURE bhv OF Avalon_master IS
 
 BEGIN
 
-process(nReset, clk)
+process(AM_nReset, clk)
 begin
-	if nReset = '0' then
+	if AM_nReset = '0' then
 		reg_SM_state <= WaitData;
 		iRegCounterAddress <= (others => '0');
 		reg_burstcount <= 0;
@@ -84,7 +84,7 @@ begin
 	end if;
 end process;
 
-process(iRegCounterAddress, reg_SM_state, FIFO_number_words, iRegAlmostEmpty, AS_Start, FIFO_Data, AS_Start_Address, AM_waitrequest, AS_Length, reg_burstcount)
+process(iRegCounterAddress, reg_SM_state, AM_UsedWords, iRegAlmostEmpty, AM_Start, AM_FIFOData, AS_StartAddress, AM_waitrequest, AM_Length, reg_burstcount)
 begin
 	next_iRegCounterAddress <= iRegCounterAddress;
 	next_reg_SM_state <= reg_SM_state;
@@ -95,9 +95,9 @@ begin
 	AM_data <= (others => '0');
 	AM_burstcount <= (others => '0');
 	FIFO_read_access <= '0';
-	AS_Status <= '0';
+	AM_Status <= '0';
 	
-	if unsigned(FIFO_number_words) > 3 then
+	if unsigned(AM_UsedWords) > 3 then
 		iRegAlmostEmpty <= '1';
 	else
 		iRegAlmostEmpty <= '0';
@@ -105,14 +105,14 @@ begin
 
 	case reg_SM_state is
 		when WaitData =>
-			if iRegAlmostEmpty = '1' AND AS_Start = '1' then
+			if iRegAlmostEmpty = '1' AND AM_Start = '1' then
 				next_reg_SM_state <= STATE_BURSTCOUNT;
 			end if;
 			
 		when STATE_BURSTCOUNT =>
 			AM_burstcount <= std_logic_vector(to_unsigned(BURSTCOUNT_LENGTH, AM_burstcount'length));
-			AM_data <= FIFO_Data;
-			AM_addr <= std_logic_vector(unsigned(AS_Start_Address) + unsigned(iRegCounterAddress));
+			AM_data <= AM_FIFOData;
+			AM_addr <= std_logic_vector(unsigned(AS_StartAddress) + unsigned(iRegCounterAddress));
 			AM_write <= '1';
 			
 			if AM_waitrequest = '0' then
@@ -122,8 +122,8 @@ begin
 			
 		when Burst =>
 		AM_write <= '1';
-		AM_addr <= std_logic_vector(unsigned(AS_Start_Address) + unsigned(iRegCounterAddress));
-		AM_data <= FIFO_Data;
+		AM_addr <= std_logic_vector(unsigned(AS_StartAddress) + unsigned(iRegCounterAddress));
+		AM_data <= AM_FIFOData;
 		
 		if AM_WaitRequest = '0' then
 			FIFO_read_access <= '1';
@@ -133,9 +133,9 @@ begin
 				next_reg_SM_state <= WaitData;
 
 				next_iRegCounterAddress <= std_logic_vector (unsigned (iRegCounterAddress) + ADDR_INCREMENT); -- increase the iRegCounterAdress register
-				if iRegCounterAddress = AS_Length then -- when the iRegCounterAddress is equal to the data length (at the end of the 3 buffers), reset the counter to 0 to restart
+				if iRegCounterAddress = AM_Length then -- when the iRegCounterAddress is equal to the data length (at the end of the 3 buffers), reset the counter to 0 to restart
 					next_iRegCounterAddress <= (others => '0');
-					AS_Status <= '1'; --tell to the slave that the image is finished
+					AM_Status <= '1'; --tell to the slave that the image is finished
 				end if;
 			end if;
 		end if;
